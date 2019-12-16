2bf89779be2044d9a12f22a6a4858ba802cd22e6
Haogang Bao <haogangbao@126.com>
2019-12-16 14:38:00 -0500

    upload multi test

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CsharpLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class BaoTest
+    {
+        public BaoTest(int id, string name)
+        {
+            this.ID = id;
+            this.name = name;
+        }
+   
+
+        public  int ID { get; set; }
+        public string name { get; set; }
+    }
+}
